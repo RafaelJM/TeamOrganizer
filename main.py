3a45e9aec093eb9cecf62611a710346d2196ec93
RafaelJM <rfel.jm@gmail.com>
2024-10-15 18:14:38 -0300

    Improving the code

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import pandas as pd
 import random
 import networkx as nx
 import matplotlib.pyplot as plt
+import re
+
+max_size_class = 40
+groups_num = 5
 
 # Definição do dtype
 dtype_student = np.dtype([
     ('work', np.int8, (5,)),          # Campo para vetor de 5 inteiros
-    ('likenums', np.int8, (40,)),      # Primeiro campo para vetor de 40 inteiros
-    ('dislikenums', np.int8, (40,))       # Segundo campo para vetor de 40 inteiros
+    ('likenums', np.int8, (max_size_class,)),      # Primeiro campo para vetor de max_size_class inteiros
+    ('dislikenums', np.int8, (max_size_class,)),    # Segundo campo para vetor de max_size_class inteiros
+    ('valid', np.bool)       
 ])
+
 dtype_group = np.dtype([
     ('student_number', np.int8),
     ('f_work', np.int32),
@@ -18,21 +24,21 @@ dtype_group = np.dtype([
     ('calculated', np.bool)
 ])
 
-students = np.zeros((40),dtype=dtype_student)
-groups = np.zeros((5,40//5),dtype=dtype_group)
+students = np.zeros((max_size_class),dtype=dtype_student)
+groups = np.zeros((groups_num,int(np.ceil(max_size_class/groups_num))),dtype=dtype_group)
 groups['student_number'] -= 1
 
 
 def likeGraph(students):
     G = nx.DiGraph()
     # Adicionando arestas para os "likes" (gostos - verde)
-    for i in range(1,40):
+    for i in range(1,max_size_class):
         for like in students[i]['likenums']:
             if like:
                 G.add_edge(i, like, color='green')
     
     # Adicionando arestas para os "dislikes" (não gostos - vermelho)
-    for i in range(1,40):
+    for i in range(1,max_size_class):
         for dislike in students[i]['dislikenums']:
             if dislike:
                 G.add_edge(i, dislike, color='red')
@@ -49,12 +55,13 @@ def likeGraph(students):
     plt.title('Grafo de Gostos e Não Gostos entre Alunos')
     plt.show()
     
-def gen_data(students,folder):
-    answers = pd.read_csv("answers/ini3b.csv")
+def gen_data(students,folder = "data/answers.csv"):
+    answers = pd.read_csv(folder)
     for row in answers.iterrows():
         student = students[row[1].iloc[3]]
+        student["valid"] = True
         for i in [0,1,2]:
-            ranquing = [int(num) for sublista in [a.split(" ") for a in row[1].iloc[i+4].split(",")] for num in sublista if num.isdigit()]
+            ranquing = list(map(int, re.findall(r'\d+', row[1].iloc[i+4])))
             student[i][:len(ranquing)] += ranquing
 
 def gen_random_data(students):
@@ -62,7 +69,7 @@ def gen_random_data(students):
     for i,student in enumerate(students[1:]):
         random.shuffle(works)
         student[0] += works
-        aux = list(range(1,40))
+        aux = list(range(1,max_size_class))
         aux.pop(i)
         for j in [1,2]:
             count = 0;
@@ -73,26 +80,24 @@ def gen_random_data(students):
                 count+=1;
 
 def genpop(groups,students):
-    indexes = list(range(1,40))
+    indexes = list(range(1,max_size_class))
     for i in groups.T:
         for j in i:
             if not len(indexes):
                 return
             j["student_number"] = indexes.pop(random.randint(0, len(indexes)-1))
 
-#def fitness(groups, students):
-        
-
 gen_random_data(students)
 likeGraph(students)
 genpop(groups,students)
 
+#def fitness(groups, students):
 for group in groups:
     for student in group:
         others = group[group != student]["student_number"]
         likenums = students[student["student_number"]]["likenums"]
         dislikenums = students[student["student_number"]]["dislikenums"]
-        student["f_like"] = np.sum(40 - np.where(np.isin(likenums, others))[0])
-        student["f_dislike"] = np.sum(np.where(np.isin(dislikenums, others))[0] - 40)
+        student["f_like"] = np.sum(max_size_class - np.where(np.isin(likenums, others))[0])
+        student["f_dislike"] = np.sum(np.where(np.isin(dislikenums, others))[0] - max_size_class)
         
 np.sum(groups["f_like"], axis=1)
